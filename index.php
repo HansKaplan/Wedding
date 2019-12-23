15375e9e321575de86e822586bab5aeb292b4844
Hans <59155955+HansKaplan@users.noreply.github.com>
2019-12-23 00:56:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<title>Nosso Casamento ❤️</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2{font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+p {line-height: 2}
+.bgimg, .bgimg2 {
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+.bgimg {background-image: url("http://www.theeventsdesigners.com/wp-content/uploads/2018/05/16.-Greenery-and-white-wedding-flowers-Ferri-Photography.jpeg")}
+.bgimg2 {background-image: url("/w3images/flowers.jpg")}
+</style>
+<body>
+
+<!-- SCM Music Player http://scmplayer.co -->
+<script type="text/javascript" src="http://scmplayer.co/script.js" 
+data-config="{'skin':'http://static.tumblr.com/qmfr3ui/gx4n5p38k/basic_7.css','volume':100,'autoplay':true,'shuffle':true,'repeat':1,'placement':'top','showplaylist':false,'playlist':[{'title':'01 - Duas Metades','url':'https://www.youtube.com/watch?v=HR4ZxjGQGYY'},{'title':'02 - Pra Ter o Seu Amor','url':'https://www.youtube.com/watch?v=e7w4I6gxdao'},{'title':'03 - Pra Voc%EA Guardei o Amor','url':'https://www.youtube.com/watch?v=NM200mNbLP8'},{'title':'04 - Photograph','url':'https://www.youtube.com/watch?v=qgmXPCX4VzU'},{'title':'05 - Perfect','url':'https://www.youtube.com/watch?v=iKzRIweSBLA'},{'title':'06 - All of Me','url':'https://www.youtube.com/watch?v=R21EU8SKUM0'},{'title':'07 - I%27m Yours','url':'https://www.youtube.com/watch?v=EkHTsc9PU2A'},{'title':'09 - De Janeiro a Janeiro','url':'https://www.youtube.com/watch?v=ObE3KzCzp0s'},{'title':'10 - Dois Cora%E7%F5es','url':'https://www.youtube.com/watch?v=vZhwva-AisM'},{'title':'11 - Sei','url':'https://www.youtube.com/watch?v=aWVGIlaohKM'},{'title':'12 - Anjo','url':'https://www.youtube.com/watch?v=uQZGJBhO1WU'},{'title':'13 - Could I Love You Any More?','url':'https://www.youtube.com/watch?v=6oQ5AXLCk-U'},{'title':'14 - Do I Wanna Know','url':'https://www.youtube.com/watch?v=bpOSxM0rNPM'},{'title':'15 - Wonderwall','url':'https://www.youtube.com/watch?v=Y7I1nCcwJP0'},{'title':'16 - Como %C9 Grande o Meu Amor por Voc%EA','url':'https://www.youtube.com/watch?v=I55sQmI95p0'},{'title':'17 - Nosso N%F3(s)','url':'https://www.youtube.com/watch?v=dwnDZpJsWmE'},{'title':'18 - Eu Me Lembro','url':'https://www.youtube.com/watch?v=olbbAuQUZd4'},{'title':'19 - Partilhar','url':'https://www.youtube.com/watch?v=ivHE7pQEEHI'},{'title':'20 - Marry Me','url':'https://www.youtube.com/watch?v=_f3fjRrXPbY'},{'title':'21 - The Reason','url':'https://www.youtube.com/watch?v=e5t_kmJT8xo'},{'title':'22 - God Only Knows','url':'https://www.youtube.com/watch?v=1s1Sc_FeE4s'},{'title':'23 - I Will Spend My Whole Life Loving You','url':'https://www.youtube.com/watch?v=3dVhZuyltII'},{'title':'24 - INK','url':'https://www.youtube.com/watch?v=lKKIE5JX1zM'},{'title':'25 - Ainda Bem','url':'https://www.youtube.com/watch?v=QHEgExz6KJg'}]}" ></script>
+<!-- SCM Music Player script end -->
+
+<!-- Header / Home-->
+<header class="w3-display-container w3-wide bgimg w3-grayscale-min" id="Inicio">
+  <div class="w3-display-middle w3-text-white w3-center">
+    <h1 class="w3-jumbo">Fernanda & Felipe</h1>
+    <h2>Nosso Casamento</h2>
+    <h2><b>05.07.2020</b></h2>
+  </div>
+</header>
+
+<!-- Navbar (sticky bottom) -->
+<div class="w3-bottom w3-hide-small">
+  <div class="w3-bar w3-white w3-center w3-padding w3-opacity-min w3-hover-opacity-off">
+    <a href="#Inicio" style="width:25%" class="w3-bar-item w3-button">Inicio</a>
+    <a href="#us" style="width:25%" class="w3-bar-item w3-button">Fernanda & Felipe</a>
+    <a href="#wedding" style="width:25%" class="w3-bar-item w3-button">Casamento</a>
+	<a href="#rsvp" style="width:25%" class="w3-bar-item w3-button">Reservas</a>
+  </div>
+</div>
+
+<!-- About / Fernanda And Felipe -->
+<div class="w3-container w3-padding-64 w3-pale-red w3-grayscale-min" id="us">
+  <div class="w3-content">
+    <h1 class="w3-center w3-text-grey"><b>Nós ❤️</b></h1>
+    <img class="w3-round w3-grayscale-min" src="https://scontent.fmoc2-1.fna.fbcdn.net/v/t1.0-9/14702377_878673272234881_4058717953296083684_n.jpg?_nc_cat=109&_nc_oc=AQlXvTmjGf0-AQAmr6IOzELlqBMPZ-Ptui41Ryq7cR641ogJMxFq6pWQUa1lVLYmysw&_nc_ht=scontent.fmoc2-1.fna&oh=c33d0e93cd42340c92c75f34eb4375a2&oe=5E5A07C7" style="width:100%;margin:32px 0">
+    
+
+	<p><i>My vows – Felipe</i></p>
+	
+	<p><i>
+
+Pra falar do amor de verdade,
+Vou começar pela melhor metade
+E te mostrar tudo de bom que tenho,
+E se for preciso eu desenho;
+Que eu amo você, que eu quero você.
+
+A outra metade é defeito,
+Você vai saber de qualquer jeito
+Anjo ou animal, suave ou fatal;
+O que de um grande amor se espera
+É que tenha fogo;
+Que domine o pensamento
+E traga sentido novo.
+
+Que tenha paixão, desejo,
+Que tenha abraço e beijo,
+E seja a melhor sensação;
+Que preencha a vida vazia,
+Mande embora a agonia,
+E que traga paz pro coração.
+
+É você, é você...
+Que preenche a vida vazia
+Manda embora a agonia
+E que trouxe paz pro coração;
+Que preenche a vida vazia,
+Manda embora a agonia,
+E que é dona do meu coração... É você
+
+<p><i> 
+
+Meu amor, eu posso fechar os olhos e consigo enxergar exatamente cada detalhe da primeira vez que encontrei você pessoalmente. Chovia naquela noite e eu estava a caminho de Belo Horizonte, quase embarcando no ônibus das 21H, foi quando te avistei ao lado de uma das plataformas, parada e me observando, assim como eu o fiz e essa foi a primeira vez que nossos olhares se cruzaram, você estava linda, tentando disfarçar que não estava me encarando e ao mesmo tempo não hesitava em me observar. A partir daquele dia, de alguma forma, eu sabia que algo havia acontecido e eu só não sabia ainda o que era, mas conseguia sentir. Com o passar dos meses, nos encontramos pela segunda vez, em outro ônibus haha parece que viagens e ônibus são duas coisas que sempre vão estar ligados à nossa história de amor. Eu, muito tímido e bobo, não me aproximei de você, até que certa feita você me mandou uma mensagem no Facebook com um: “Oi, eu te vi lá no ônibus”. Desde esse dia, a minha vida nunca mais foi a mesma porque eu sentia algo diferente cada vez que trocávamos mensagens, desde o Oi até o Boa Noite que eu não queria dizer porque no meu coração, eu já sentia que era você a minha certeza, o meu amor. Foram longos meses nos conhecendo até que eu tomasse coragem e fosse até você e finalmente, te pedir em namoro.
+
+Hoje, já são 5 anos, quase 6, juntos. E eu não tenho qualquer dúvida, assim como não tive anteriormente quando a pedi em namoro, de que você, Fernanda Santos Pereira Castro, é o maior presente enviado pelo Pai para completar a minha felicidade, é muito mais do que eu sonhava ou imaginava e pedi a Deus em orações para colocar em minha vida e ao meu lado para todo o sempre, é a mulher da minha vida. Além da noiva maravilhosa que você é, é também a minha melhor amiga, a minha companheira de toda a vida. É a pessoa com quem posso contar a qualquer momento porque sei que estará lá por mim sempre, assim como eu estarei para sempre por ti, meu amor. Em qualquer situação que eu esteja enfrentando, quando me sinto desanimado e quero desistir dos meus projetos é você quem me aconselha, que me dá bronca quando preciso, quem cuida de mim quando estou doente ou não estou bem, que me oferece um abraço e um ombro para repousar, com quem eu posso desabafar quando as coisas não vão bem e parece que não irão melhorar. Você me inspira a crescer, a buscar ser sempre o melhor que eu posso ser, me enxerga além das minhas falhas e tropeços e acredita em mim quando nem mesmo eu o faço. Eu amo poder acordar e ler as suas mensagens de "Bom Dia Meu Amor", de podermos sair de mãos dadas. Eu amo escutar você me dizendo sobre o quanto esperou e sonhou com a vida que conquistamos hoje, eu amo admirar os seus mínimos detalhes e contemplar o quanto és linda, eu amo estar te observar e admirar quando estamos conversando e você acaba dormindo sem nem ao menos me responder, eu amo poder ouvir as suas gargalhadas com alguma coisa engraçada que eu disse ou simplesmente sorri ao me encontrar depois de 5 dias, ou 5 minutos. Eu amo poder passar um dia inteiro com você, porque já visualizo em minha mente como serão os nossos dias a partir de hoje e fico triste por não poder fazê-lo todos os dias por ter de nos despedirmos.
+Ao seu lado eu consigo ver além do que os meus olhos enxergam, você me faz acreditar de que sonhar vale a pena porque se hoje estou aqui escrevendo esses votos para o nosso casamento foi porque um dia, Deus olhou para mim e decidiu que era hora de tornar um dos meus maiores sonhos, realidade: de encontrar a minha companheira e alma gêmea. Me imagino daqui a 50 anos, sentado em uma varanda ao seu lado, numa xácara, com todos os nossos filhos e netos ao nosso redor, dizendo o quanto são felizes por contemplarem a nossa felicidade, mesmo após tanto tempo juntos e o nosso amor continua firme e forte. As nossas viagens ao redor do Brasil e o mundo, celebrando o nosso casamento a cada ano comemorando as nossas bodas de alguma coisa, porque eu nunca vou me lembrar qual será, mas prometo me esforçar para aprender.
+
+Deus nos uniu para todo o sempre, nos abençoou com graça e amor, para que possamos ser felizes juntos, onde um completa o outro. Eu esperei ansiosamente por este dia, e hoje aqui estamos, realizando mais um dos nossos sonhos. Nunca houve dois corações mais abertos, nem gostos mais semelhantes ou sentimentos com maior sintonia quanto nós dois. 
+
+Diante de Deus e dos homens, dos nossos familiares e amigos que testemunham e compartilham conosco deste momento tão ímpar e especial em nossas vidas: Eu prometo honrar você como a mulher virtuosa que és, meu amor. Honrar o nosso casamento, respeitar você e aos nossos filhos, compreendê-la melhor, cuidar de você e protegê-la, ser um esposo amoroso e companheiro, sabendo administrar o nosso lar, educar os nossos filhos segundo os mandamentos e a vontade de Deus Pai, ensiná-los o caminho correto a ser seguido guiando-os na direção do bem. Zelar pelo nosso lar e cuidar dos nossos filhos, e dos filhos dos nossos filhos como um homem honrado, valoroso e temente a Deus deve fazer. Ser paciente e carinhoso, amoroso e parceiro, demonstrar dia após dia a gratidão e o amor que sinto por tê-la ao meu lado e poder desfrutar dos frutos do nosso amor e da nossa união. Prometo cuidar de ti, na saúde e na doença, na riqueza e na pobreza, independente de quaisquer circunstâncias até que a morte nos separe ou até que o Senhor Jesus venha e com essa aliança, prometo diante da igreja e de Deus manter as minhas promessas, em sinal do meu amor e compromisso, eu a desposo.
+</i>
+    </p><br>
+    <p class="w3-center"><a href="#wedding" class="w3-button w3-black w3-round w3-padding-large w3-large">Detalhes da Cerimônia</a></p>
+  </div>
+</div>
+
+<!-- Background photo -->
+<div class="w3-display-container bgimg2">
+  <div class="w3-display-middle w3-text-white w3-center">
+    <h1 class="w3-jumbo">Você está convidado(a)</h1><br>
+    <h2>É claro..</h2>
+	<body style='background-color:#F6DDCC'>
+  </div>
+</div>
+
+<!-- Wedding information -->
+<div class="w3-container w3-padding-64 w3-pale-red w3-grayscale-min w3-center" id="wedding">
+  <div class="w3-content">
+    <h1 class="w3-text-grey"><b>O Casamento</b></h1>
+    <img class="w3-round-large w3-grayscale-min" src="https://scontent.fmoc2-1.fna.fbcdn.net/v/t1.0-9/418562_342168592533772_1213183734_n.jpg?_nc_cat=109&_nc_ohc=6qvV7bV72JkAQmL37I-aAaUcNewTfgXRHNCC7J55bPV24NPD2lBfBTq9Q&_nc_ht=scontent.fmoc2-1.fna&oh=5c3ae34529475c8ce7fb0580b0c2d8f0&oe=5E58260F" style="width:100%;margin:64px 0">
+    <div class="w3-row">
+      <div class="w3-half">
+        <h2>Horários</h2>
+        <p>Cerimônia de Casamento - 19 H</p>
+        <p>Jantar e Recepção - 22 H</p>
+      </div>
+      <div class="w3-half">
+        <h2>Local</h2>
+        <p>Rua Jésus Chateaubriand de Souza, Nº 2014 Coração de Jesus - MG</p>
+        <p>Espaço Maria Cândida</p>
+		
+		<p>Onde os noivos receberam os cumprimentos dos presentes.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- RSVP section -->
+<div class="w3-container w3-padding-64 w3-pale-red w3-center w3-wide" id="rsvp">
+  <h1>Contamos com a sua presença!</h1>
+  <p class="w3-large"></p>
+  <p class="w3-xlarge">
+    <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-round w3-red w3-opacity w3-hover-opacity-off" style="padding:8px 60px">Reservas</button>
+  </p>
+</div>
+
+<!-- RSVP modal -->
+<div id="id01" class="w3-modal">
+  <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="padding:32px;max-width:600px">
+    <div class="w3-container w3-white w3-center">
+      <h1 class="w3-wide"></h1>
+      <p>Realmente aguardamos a sua presença.</p>
+      <form>
+        <input class="w3-input w3-border" type="text" placeholder="Nome(s)" name="name">
+      </form>
+      <p><i>Atenciosamente, Fernanda & Felipe</i></p>
+      <div class="w3-row">
+        <div class="w3-half">
+          <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-block w3-green">Comparecerei</button>
+        </div>
+        <div class="w3-half">
+          <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-block w3-red">Não poderei ir</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <p>Desenvolvido por <a  title="André Felipe Castro" target="_blank" class="w3-hover-text-green">André Felipe Castro</a></p>
+</footer>
+<div class="w3-hide-small" style="margin-bottom:32px"> </div>
+
+</body>
+</html>
